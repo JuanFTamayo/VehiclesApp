069bc17fad8e46bcffc66c4e64443a8796a670c0
JuanFTamayo <88947668+JuanFTamayo@users.noreply.github.com>
2021-10-04 18:27:28 -0500

    begin procedure screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -63,24 +63,24 @@ bool _showLoader= false;
 
   Widget _showEmail() {
     return Container(
-      padding: EdgeInsets.all(10),
-      child: TextField(
-        autofocus: true,
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-         hintText: 'Ingresa tu email...' ,
-         labelText: 'Email',
-         errorText: _emailShowError? _emailError: null,
-         prefixIcon: Icon(Icons.alternate_email),
-         suffixIcon: Icon(Icons.email),
-         border: OutlineInputBorder(
-           borderRadius: BorderRadius.circular(10) )
-        ),
-         onChanged: (value){
-        _email= value;
+        padding: EdgeInsets.all(10),
+        child: TextField(
+          autofocus: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'Ingresa tu email...' ,
+            labelText: 'Email',
+            errorText: _emailShowError? _emailError: null,
+            prefixIcon: Icon(Icons.alternate_email),
+            suffixIcon: Icon(Icons.email),
+           border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10) )
+          ),
+          onChanged: (value){
+          _email= value;
         
-      }
-      ),
+          }
+        ),
      
     );
   }
